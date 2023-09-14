1e8184c45d38d818f95f04e7ead6c818f5d422f9
chungangtsao <tsao.c@northeastern.edu>
2023-09-14 16:44:05 -0700

    completed rectangle.py

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractclassmethod
+
 """
  Sample modified from CS5500, Mike Shah
 
@@ -5,13 +7,22 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
+class Shape(ABC):
+    @abstractclassmethod
+    def set_value(self,width,height):
+        pass
+
+    @abstractclassmethod
+    def area(self):
+        pass
+
 class Rectangle:
-    def set_values(self, x, y):
-        self.width = x
-        self.height = y
+    def set_values(self, width, height):
+        self._width = width
+        self._height = height
 
     def area(self):
-        return self.width * self.height
+        return self._width * self._height
 
 
 if __name__ == "__main__":
