113acd37a88da34cabf4caa2876b03de87244007
chungangtsao <tsao.c@northeastern.edu>
2023-10-26 18:52:30 -0700

    upload with new gitignore

--- Lab6/todo_app/src/main.py
+++ Lab6/todo_app/src/main.py
@@ -2,41 +2,51 @@ from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-#GET ReST API curl 'http://localhost:8000/api'
-@app.get("/api") #transform func into rest api (get) 
-def first_api():
-    return {"msg":"This is Get Rest API"}
+# GET ReST API curl 'http://localhost:8000/api'
 
-#GET ReST API with path parameters curl 'http://localhost:8000/stuff/random_stuff'
+
+@app.get("/api")  # transform func into rest api (get)
+def first_api():
+    return {"msg": "This is Get Rest API"}
+
+# GET ReST API with path parameters curl 'http://localhost:8000/stuff/random_stuff'
+
+
 @app.get("/stuff/{path_param}")
-def first_api2(path_param:str):
+def first_api2(path_param: str):
     return {"msg": f"path used is {path_param}"}
 
-#GET ReST API with query parameters curl 'http://localhost:8000/stuff/?name=cool_stuff'
+# GET ReST API with query parameters curl 'http://localhost:8000/stuff/?name=cool_stuff'
+
+
 @app.get("/stuff/")
-def first_api3(name:str):
+def first_api3(name: str):
     return {"msg": f"category name of stuff is {name}"}
 
-#GET ReST API with path parameters and query parameters curl 'http://localhost:8000/stuff/name_two/?id=5'
+# GET ReST API with path parameters and query parameters curl 'http://localhost:8000/stuff/name_two/?id=5'
+
+
 @app.get("/stuff/{category_name}/")
-def first_api4(category_name:str, id:int):
+def first_api4(category_name: str, id: int):
     return {"msg": f"path or category name used is {category_name} and queried by id number: {id}"}
 
-#Create a POST ReST API curl 'http://localhost:8000/stuff/create_stuff/' -d "{'name':'first', 'category':'random_stuff','id':'1'}"
-@app.post("/stuff/create_stuff/") 
+# Create a POST ReST API curl 'http://localhost:8000/stuff/create_stuff/' -d "{'name':'first', 'category':'random_stuff','id':'1'}"
+
+
+@app.post("/stuff/create_stuff/")
 def first_api5(new_stuff=Body()):
     print(new_stuff)
-    return {"msg":new_stuff}
+    return {"msg": new_stuff}
 
-#Create a PUT ReST API curl -X PUT 'http://localhost:8000/stuff/update_stuff/first' -d "{'name':'updated_name', 'category':'new_category', 'id':1}"
+# Create a PUT ReST API curl -X PUT 'http://localhost:8000/stuff/update_stuff/first' -d "{'name':'updated_name', 'category':'new_category', 'id':1}"
+
 
 @app.put("/stuff/update_stuff/{name}")
 def first_api6(updated_stuff=Body()):
     return {"msg": f"Item is updated with data: {updated_stuff}"}
 
 
-
-#Create a DELETE ReST API curl -X DELETE 'http://localhost:8000/stuff/delete_stuff/?id=1'
+# Create a DELETE ReST API curl -X DELETE 'http://localhost:8000/stuff/delete_stuff/?id=1'
 @app.delete("/stuff/delete_stuff/")
-def first_api7(id:int):
-    return {"msg":f"item with id number: {id} is deleted."}
+def first_api7(id: int):
+    return {"msg": f"item with id number: {id} is deleted."}
--- Lab6/todo_app/tests/test_main.py
+++ Lab6/todo_app/tests/test_main.py
@@ -1,22 +1,21 @@
 from ..src.main import first_api, first_api2, first_api3, first_api4, first_api5, first_api6, first_api7
 
+
 def test_first_api():
     response = first_api()
     assert response["msg"] == "This is Get Rest API"
-    # Add more assertions if needed
+
 
 def test_first_api2():
     response = first_api2("random_stuff")
     assert response["msg"] == "path used is random_stuff"
-    # Add more assertions if needed
+
 
 def test_first_api3():
     response = first_api3("cool_stuff")
     assert response["msg"] == "category name of stuff is cool_stuff"
-    # Add more assertions if needed
+
 
 def test_first_api4():
     response = first_api4("name_two", 5)
     assert response["msg"] == "path or category name used is name_two and queried by id number: 5"
-    # Add more assertions if needed
-
